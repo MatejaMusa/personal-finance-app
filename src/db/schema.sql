3e608c305cd166750923391abbaf1636b235afdf
MatejaMusa <matejamusa6@gmail.com>
2024-08-22 08:39:25 +0200

    update sql file

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -61,3 +61,39 @@ CREATE TABLE Transactions
     FOREIGN KEY (account_id) REFERENCES Accounts (id) ON DELETE RESTRICT ON UPDATE CASCADE,
     FOREIGN KEY (category_id) REFERENCES Categories (id) ON DELETE RESTRICT ON UPDATE CASCADE
 );
+
+INSERT INTO Users (username, password) VALUES ('Matejamusa', 'Matejamusa123!');
+
+INSERT INTO Categories (name, type) VALUES
+                                        ('Namirnice', 'EXPENSE'),
+                                        ('Racuni', 'EXPENSE'),
+                                        ('Plata', 'INCOME'),
+                                        ('Vecera', 'EXPENSE'),
+                                        ('Prevoz', 'EXPENSE'),
+                                        ('Zabava', 'EXPENSE'),
+                                        ('Pokloni', 'INCOME'),
+                                        ('Investicije', 'INCOME');
+
+INSERT INTO UserCategories (user_id, category_id)
+SELECT 1, id FROM Categories;
+
+INSERT INTO Accounts (user_id, name, description, priority, balance) VALUES
+                                                                         (1, 'Glavni racun', 'Glavni racun za trosenje', 'HIGH', -11500.00),
+                                                                         (1, 'Stedni racun', 'Racun za stednju', 'MEDIUM', 15000.00),
+                                                                         (1, 'Kreditna kartica', 'Racun za kreditnu karticu', 'LOW', -7000.00);
+
+INSERT INTO Transactions (account_id, category_id, amount, transaction_date) VALUES
+                                                                                 (1, 1, 1000.00, '2024-08-01 10:00:00'),
+                                                                                 (1, 2, 6000.00, '2024-08-02 09:00:00'),
+                                                                                 (1, 4, 2000.00, '2024-08-05 09:00:00'),
+                                                                                 (1, 5, 1500.00, '2024-08-07 19:45:00'),
+                                                                                 (1, 6, 1000.00, '2024-08-10 08:15:00');
+
+INSERT INTO Transactions (account_id, category_id, amount, transaction_date) VALUES
+                                                                                 (2, 7, 5000.00, '2024-08-06 16:00:00'),
+                                                                                 (2, 8, 10000.00, '2024-08-15 11:30:00');
+
+INSERT INTO Transactions (account_id, category_id, amount, transaction_date) VALUES
+                                                                                 (3, 2, 4000.00, '2024-08-12 20:00:00');
+                                                                                 (3, 6, 1000.00, '2024-08-18 15:45:00'),
+                                                                                 (3, 1, 2000.00, '2024-08-20 11:00:00');
